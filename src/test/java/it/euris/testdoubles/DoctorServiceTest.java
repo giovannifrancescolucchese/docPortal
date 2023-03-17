395a8ca646ce571c4b0d2343fd72a815ee241b53
LattaLuca00 <llattanzio87@gmail.com>
2023-03-17 14:31:15 +0100

    First Commit Luca Lattanzio

--- src/test/java/it/euris/testdoubles/DoctorServiceTest.java
+++ src/test/java/it/euris/testdoubles/DoctorServiceTest.java
@@ -1,7 +1,5 @@
 package it.euris.testdoubles;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import it.euris.model.Doctor;
 import it.euris.model.Patient;
 import it.euris.model.PressureDevice;
@@ -21,35 +19,44 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class DoctorServiceTest {
 
     PatientService patientService;
+    PatientService patientServiceStubFalse;
+    PatientService patientServiceStubTrue;
     DoctorService doctorService;
 
     PressureDeviceService pressureDeviceService;
     DoctorMatrixService doctorMatrixService;
+    DoctorMatrixServiceDummy doctorMatrixServiceDummy;
+
 
     @BeforeEach
     void setup() {
 
         //patientService=new PatientServiceStub(false);
-        //patientService=new PatientServiceStub(false);
+        patientServiceStubFalse=new PatientServiceStub(false);
+        patientServiceStubTrue=new PatientServiceStub(true);
         pressureDeviceService=new PressureDeviceServiceDummy();
         patientService=new PatientServiceImpl(pressureDeviceService);
-        doctorMatrixService=new DoctorMatrixServiceDummy();
+        doctorMatrixServiceDummy=new DoctorMatrixServiceDummy();
+        doctorMatrixService=new DoctorMatrixServiceImpl();
     }
 
     //STUB
     @Test
     void givenPatientNoResponseThenPatientsNoResponseListIsNotEmpty() {
         //arrange
-        doctorService=new DoctorServiceImpl(patientService, doctorMatrixService);
+        doctorService = new DoctorServiceImpl(patientServiceStubFalse, doctorMatrixServiceDummy);
 
-        PressureLog log=new PressureLog(1L, LocalDate.now().minusDays(10), 123);
-        List<PressureLog> logs=new ArrayList<>();
+        PressureLog log = new PressureLog(1L, LocalDate.now().minusDays(10), 123);
+        List<PressureLog> logs = new ArrayList<>();
         logs.add(log);
-        PressureDevice pressureDevice=new PressureDevice(1L, logs);
-        Patient patient=new Patient(
+        PressureDevice pressureDevice = new PressureDevice(1L, logs);
+        Patient patient = new Patient(
                 new Long(1),
                 "nome",
                 "cognome",
@@ -59,19 +66,86 @@ public class DoctorServiceTest {
                 LocalDate.of(1915, Month.JULY, 29),
                 pressureDevice
         );
-        List<Patient> patientList=new ArrayList<>();
+        List<Patient> patientList = new ArrayList<>();
         patientList.add(patient);
-        Doctor doctor=new Doctor(1L, "nomeDottore","cognomeDotttore","Via Garibaldi 1", "dottore@email.it", patientList, true);
+        Doctor doctor = new Doctor(1L, "nomeDottore", "cognomeDotttore", "Via Garibaldi 1", "dottore@email.it", patientList, true);
         //act
-        List patientsNoResponse=doctorService.getPatientsNoResponse(doctor);
+        List patientsNoResponse = doctorService.getPatientsNoResponse(doctor);
         //assert
         assertFalse(patientsNoResponse.isEmpty());
-        assertEquals(patientsNoResponse.size(),1);
+        assertEquals(patientsNoResponse.size(), 1);
 
     }
 
+    @Test
+    void givenPatientOutOfRangeThenPatientMeasuresAreOutOfRangeListIsNotEmpty() {
+        doctorService = new DoctorServiceImpl(patientServiceStubTrue, doctorMatrixServiceDummy);
+       /* PressureLog log = new PressureLog(1L, LocalDate.now().minusDays(10), 154);
+        PressureLog log1 = new PressureLog(3L, LocalDate.now().minusDays(9), 164);
+        PressureLog log2 = new PressureLog(2L, LocalDate.now().minusDays(8), 152);
+        List<PressureLog> logs = new ArrayList<>();
+        logs.add(log);
+        logs.add(log1);
+        logs.add(log2);
+        PressureDevice pressureDevice = new PressureDevice(1L, logs);*/
+        Patient patient = new Patient(
+                new Long(1),
+                "nome",
+                "cognome",
+                "indirizzo",
+                "email",
+                'M',
+                LocalDate.of(1915, Month.JULY, 29),
+                null
+        );
+        List<Patient> patientList = new ArrayList<>();
+        patientList.add(patient);
+        Doctor doctor = new Doctor(1L, "nomeDottore", "cognomeDotttore", "Via Garibaldi 1", "dottore@email.it", patientList, true);
+        List<Patient> patientsOutOfRange = doctorService.getPatientsOutOfRange(doctor);
+        assertFalse(patientsOutOfRange.isEmpty());
+        assertEquals(1, patientsOutOfRange.size());
+    }
 
+    @Test
+    void given51PatientsWithAgeOver70ThenAddThe51PatientAtTheDoctorResponseIsFalse() {
+        doctorService = new DoctorServiceImpl(patientServiceStubFalse, doctorMatrixService);
+        PressureDevice pressureDevice = null;
+        List<Patient> patientsList=new ArrayList<>();
+        Doctor doctor = new Doctor(1L, "nomeDottore", "cognomeDotttore", "Via Garibaldi 1", "dottore@email.it", patientsList, true);
 
+        for (int i = 0; i <= 49; i++) {
+            pressureDevice = new PressureDevice(new Long(i), new ArrayList<>());
+            LocalDate startMeasures = LocalDate.of(2022, Month.MARCH, 16);
+            for (int j = 1; j <= 365; j++) {
+                PressureLog pressureLog = new PressureLog(new Long(j), startMeasures, 130);
+                pressureDevice.getPressureLogs().add(pressureLog);
+                startMeasures = startMeasures.plusDays(1L);
+            }
+            Patient patient = new Patient(
+                    new Long(i),
+                    "nome",
+                    "cognome",
+                    "indirizzo",
+                    "email",
+                    'M',
+                    LocalDate.of(1920, Month.JULY, 29),
+                    pressureDevice
+            );
 
+            doctorService.addPatientToDoctor(patient, doctor);
+        }
+
+        Boolean result = doctorService.addPatientToDoctor(new Patient(50L,
+                "nome",
+                "cognome",
+                "indirizzo",
+                "email",
+                'M',
+                LocalDate.of(2000, Month.JULY, 29),
+                pressureDevice
+        ), doctor);
+
+        assertEquals(Boolean.FALSE, result);
 
     }
+}
--- src/test/java/it/euris/testdoubles/PatientServiceTest.java
+++ src/test/java/it/euris/testdoubles/PatientServiceTest.java
@@ -10,15 +10,22 @@ import it.euris.service.patient.PatientServiceImpl;
 import it.euris.service.pressureDevice.PressureDeviceService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoSettings;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PatientServiceTest {
 
     PatientService patientService;
-    PressureDeviceService pressureDeviceService;
+    PressureDeviceServiceDummy pressureDeviceService;
+
+    @Mock
+    PressureDeviceService pressureDevice;
 
     @BeforeEach
     void setup() {
@@ -48,5 +55,45 @@ public class PatientServiceTest {
         assertFalse(patientService.haveResponsefromDeviceinLast5days(patient));
     }
 
+    @Test
+    void given3LastMeasuresOutOfRangeFromPatientOver70MaleThenReturnTrue(){
+        PressureDevice pressureDevice=new PressureDevice(new Long(1),new ArrayList<>());
+        PressureLog pressureLog=new PressureLog(new Long(1), LocalDate.now().minusDays(3L),200);
+        PressureLog pressureLog1=new PressureLog(new Long(2), LocalDate.now().minusDays(2L),180);
+        PressureLog pressureLog2=new PressureLog(new Long(3), LocalDate.now().minusDays(1L),160);
+        List<PressureLog> pressureLogs=pressureDevice.getPressureLogs();
+        pressureLogs.add(pressureLog);
+        pressureLogs.add(pressureLog1);
+        pressureLogs.add(pressureLog2);
+        Patient patient=new Patient(
+                new Long(1),
+                "nome",
+                "cognome",
+                "indirizzo",
+                "email",
+                'M',
+                LocalDate.of(1915, Month.JULY, 29),
+                pressureDevice
+        );
+
+        patientService=new PatientServiceImpl(pressureDeviceService);
+
+        assertTrue(patientService.last3MeasuresOutOfRange(patient));
+    }
+
+    @Test
+    void givenListPressureLogsAfterOneYearThenAfterEliminationTheListIsEmpty(){
+        PressureDevice pressureDevice=new PressureDevice(new Long(1),new ArrayList<>());
+        PressureLog pressureLog=new PressureLog(new Long(1), LocalDate.now().minusYears(3L),200);
+        PressureLog pressureLog1=new PressureLog(new Long(2), LocalDate.now().minusDays(2L),180);
+        PressureLog pressureLog2=new PressureLog(new Long(3), LocalDate.now().minusDays(2L),160);
+        List<PressureLog> pressureLogs=pressureDevice.getPressureLogs();
+        pressureLogs.add(pressureLog);
+        pressureLogs.add(pressureLog1);
+        pressureLogs.add(pressureLog2);
+
+        patientService=new PatientServiceImpl(this.pressureDevice);
+      //  Mockito
+    }
 
 }
