792a568598cbbd378215a531ced823051007b80c
Ionut Adrian <gavrilaionutadrian@gmail.com>
2023-03-17 14:29:17 +0100

    prova commit

--- src/test/java/it/euris/testdoubles/DoctorMatrixServiceDummy.java
+++ src/test/java/it/euris/testdoubles/DoctorMatrixServiceDummy.java
@@ -5,8 +5,9 @@ import it.euris.model.Patient;
 import it.euris.service.doctorMatrix.DoctorMatrixService;
 
 public class DoctorMatrixServiceDummy implements DoctorMatrixService {
+
     @Override
     public boolean canAddPatientToDoctor(Patient patient, Doctor doctor) {
-        return false;
+        return false;//TODO
     }
 }
