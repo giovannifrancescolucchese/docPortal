5cfc31e82a0431355c8613fb7bdbe20a771a17bf
Giovanni F. Lucchese <info@lucchese.name>
2023-09-05 11:21:19 +0200

    parametrized test DoctorMatrixServiceImplTest

--- src/test/java/it/euris/service/doctorMatrix/DoctorMatrixServiceImplTest.java
+++ src/test/java/it/euris/service/doctorMatrix/DoctorMatrixServiceImplTest.java
@@ -6,6 +6,8 @@ import it.euris.model.PressureDevice;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 
 import java.time.LocalDate;
@@ -40,6 +42,31 @@ class DoctorMatrixServiceImplTest {
         //assert
         assertTrue(result);
     }
+
+
+    @ParameterizedTest(name="Given {0} patients of {1} years average age when try to add a patient of {2} years then return {3}  ")
+    @CsvSource({
+            "0,  0, 0, true",
+            "80, 45, 23, false"
+    })
+    void parametrizedTestCanAddPatientToDoctor(int numPatients, int averagePatientAge, int ageNewPatient, boolean expectedResult) {
+        //arrange
+        List<Patient> pazienti=new ArrayList<>();
+        for (int i=1;i<=numPatients;i++) {
+            pazienti.add(new Patient(0L,"","","","",'M', LocalDate.now().minusYears(averagePatientAge), pressureDevice)
+            );
+        }
+        Doctor doctor=new Doctor(0L,"","","","",pazienti,true);
+        Patient newPatient=new Patient(0L,"","","","",'M', LocalDate.now().minusYears(ageNewPatient), pressureDevice);
+
+        boolean result = false;
+        DoctorMatrixService doctorMatrixService=new DoctorMatrixServiceImpl();
+        //act
+        result=doctorMatrixService.canAddPatientToDoctor(newPatient, doctor);
+        //assert
+        assertEquals(result,expectedResult);
+    }
+
 
 
 
